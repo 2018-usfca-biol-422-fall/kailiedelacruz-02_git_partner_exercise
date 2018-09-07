173ee787bf463d30cb3a883d43ab705743f5f849
Kailie Dela Cruz <kmdelacruz@dons.usfca.edu>
2018-09-07 16:22:27 -0700

    Print out sequence names

--- sequence_data.sh
+++ sequence_data.sh
@@ -12,3 +12,14 @@ mkdir code data data/raw_data output
 echo "Downloading sequence data"
 curl -L https://npk.io/NB6Y5a+ -o data/raw_data/sequence_data.zip
 unzip data/raw_data/sequence_data.zip -d data/raw_data
+
+# Pull out all of the sequence names from the fasta file
+# and put them into a text file in the output folder
+echo "Extracting sequence names from animal mRNA fasta"
+grep ">" data/raw_data/animal_mRNA.fasta > output/animal_sequence_names.txt
+echo "Extracting sequence names from crab mRNA fasta"
+grep ">" data/raw_data/crab_mitochondrial_rRNA.fasta > output/crab_sequence_names.txt
+echo "Extracting sequence names from invertebrates mRNA fasta"
+grep ">" data/raw_data/invertebrates_mRNA.fasta > output/invertebrates_sequence_names.txt
+echo "Extracting sequence names from plant rRNA fasta"
+grep ">" data/raw_data/plant_rRNA.fasta > output/plant_sequence_names.txt
